079dd92c0f69571dc937a202c8d8bf76fc8a09a3
hukeshbhagat <hukeshbhagat5@gmail.com>
2026-09-04 20:04:56 +0530

    Day 2 same project selfmade

--- Loginform/script.js
+++ Loginform/script.js
@@ -1,13 +1,15 @@
-function login() {
-    const username = document.getElementById("username").value
-    const password = document.getElementById("password").value
-    const meassage = document.getElementById("meassage")
+    // Day 1 03/09
 
-    if(username === "Hukesh" && password === "1234") {
-        meassage.style.color = "green";
-        meassage.textContent = "Login successfull";
-    } else {
-        meassage.style.color = "red";
-        meassage.textContent = "Invalid username or password";
-    }
-}
+// function login() {
+//     const username = document.getElementById("username").value
+//     const password = document.getElementById("password").value
+//     const meassage = document.getElementById("meassage")
+
+//     if(username === "Hukesh" && password === "1234") {
+//         meassage.style.color = "green";
+//         meassage.textContent = "Login successfull";
+//     } else {
+//         meassage.style.color = "red";
+//         meassage.textContent = "Invalid username or password";
+//     }
+// }
